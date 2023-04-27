84d2811d14c8dd986fe4697fc312cabd7b160026
Shaurya Gupta <shaurya50211@gmail.com>
2023-04-27 17:25:10 -0400

    started to add the dice

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -10,32 +10,36 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
         sceneView.delegate = self
-//        sceneView.scene = scene
         
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        let material = SCNMaterial()
+        let scene = SCNScene(named: "art.scnassets/diceCollada.scn")!
         
-        material.diffuse.contents = UIColor.red
+        let diceNode = scene.rootNode.childNode(withName: "Dice", recursively: true)
         
-        cube.materials = [material]
+        //        sceneView.scene = scene
         
-        let node = SCNNode()
-        
-        node.position = SCNVector3(x: 0, y: -0.1, z: -0.5)
-        
-        node.geometry = cube
+        //        let sphere = SCNSphere(radius: 0.2)
+        //        let material = SCNMaterial()
+        //
+        //        material.diffuse.contents = UIImage(named: "art.scnassets/earth.jpeg")
+        //
+        //        sphere.materials = [material]
+        //
+        //        let node = SCNNode()
+        //
+        //        node.position = SCNVector3(x: 0, y: -0.1, z: -0.5)
+        //
+        //        node.geometry = sphere
+        //        sceneView.scene.rootNode.addChildNode(node)
+        //
         
         sceneView.autoenablesDefaultLighting = true
-        sceneView.scene.rootNode.addChildNode(node)
     }
     
     override func viewWillAppear(_ animated: Bool) {
